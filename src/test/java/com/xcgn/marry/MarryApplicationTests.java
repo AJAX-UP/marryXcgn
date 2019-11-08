748ef7b9370426b2a72fdf6c777db480b5c2d818
czl960612 <18513348915@163.com>
2019-11-08 17:02:21 +0800

    Initial commit

---
+++ src/test/java/com/xcgn/marry/MarryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcgn.marry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
