9a5a9a9df08bbf27e1e2f6c4356bfe23eb2dbdbc
czl960612 <18513348915@163.com>
2019-11-08 18:34:04 +0800

    初始化添加

--- src/main/java/com/xcgn/marry/business/admin/LoginController.java
+++ src/main/java/com/xcgn/marry/business/admin/LoginController.java
@@ -4,23 +4,19 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Api("客服接口")
-@Controller
+@RestController
 @RequestMapping("/login")
 public class LoginController {
 
     @ApiOperation(value="测试接口api", notes="根据cid获取客服信息，this is test api")
     @ApiImplicitParam(name = "cid", value = "客户id", required = true, dataType = "String")
-    @ResponseBody
     @PostMapping("/getCenter")
-    public String getCinterCall(@PathVariable String cid) {
+    public String getCinterCall(String cid) {
 
         return "";
     }
