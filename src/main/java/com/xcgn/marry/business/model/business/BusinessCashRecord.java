0b424970ad249067519cbeb238b2f4c9e9525c89
czl960612 <18513348915@163.com>
2019-11-19 14:28:25 +0800

    提现添加备注字段

--- src/main/java/com/xcgn/marry/business/model/business/BusinessCashRecord.java
+++ src/main/java/com/xcgn/marry/business/model/business/BusinessCashRecord.java
@@ -15,6 +15,16 @@ public class BusinessCashRecord {
 
     private Integer userId;
 
+    private String remark;
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
     public Integer getId() {
         return id;
     }
